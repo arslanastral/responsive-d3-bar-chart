f973d3c0c34f8338b7515f98fb5cce51d6dbb851
Arslan <arslanastral@outlook.com>
2021-12-05 11:57:12 +0800

    added data fetching logic

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import * as d3 from "d3";
+import { BarChartContext } from "../App";
 
 const BarChart = () => {
+  const { data, setData, currentData } = useContext(BarChartContext);
+
+  useEffect(() => {
+    let parseDate = d3.timeParse("%Y");
+    const row = (d) => {
+      d.Year = parseDate(d.Year);
+      d.GDP = +d.GDP;
+      return d;
+    };
+
+    d3.csv(currentData.URL, row).then(setData);
+  }, [currentData]);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return <div></div>;
 };
 
