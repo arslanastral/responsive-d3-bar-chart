789f8f36cbe18572a046887b6983a14855a06134
Arslan <81504779+arslanastral@users.noreply.github.com>
2021-12-08 14:00:50 +0800

    added gradient bar fill

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 import "sanitize.css";
 import "sanitize.css/forms.css";
 import "sanitize.css/typography.css";
-import './index.css'
-import App from './App'
+import "animate.css";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
